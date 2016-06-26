375727deddba78e0b2cd11ee7c45cec8847634eb
Matthew King <mking@mking.net>
2016-06-26 09:38:29 +0800

    Add Base58 alphabet.

--- src/Bijection/Alphabet.cs
+++ src/Bijection/Alphabet.cs
@@ -6,17 +6,22 @@
     public static class Alphabet
     {
         /// <summary>
-        /// Base-16 / Hexadecimal.
+        /// Base-16: hexadecimal.
         /// </summary>
         public const string Base16 = "0123456789ABCDEF";
 
         /// <summary>
-        /// Base-52 / Duopentagesimal: Base62 without vowels.
+        /// Base-52: similar to base-62, but excluding vowels.
         /// </summary>
         public const string Base52 = "0123456789BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz";
 
         /// <summary>
-        /// Base-62 / Duosexagesimal: 0-9, A-Z, a-z.
+        /// Base-58: similar to base-62, but excluding characters that may look similar.
+        /// </summary>
+        public const string Base58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+        /// <summary>
+        /// Base-62: 0-9, A-Z, a-z.
         /// </summary>
         public const string Base62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     }
