f9f23135c12acd4214340e4aac17ca34baf58480
Matthew King <mking@mking.net>
2021-12-23 13:02:19 +0800

    Convert to file-scoped namespaces.

--- src/Bijection/Alphabet.cs
+++ src/Bijection/Alphabet.cs
@@ -1,33 +1,32 @@
-﻿namespace Bijection
+﻿namespace Bijection;
+
+/// <summary>
+/// Standard alphabets commonly used in bijective encoding/decoding functions.
+/// </summary>
+public static class Alphabet
 {
     /// <summary>
-    /// Standard alphabets commonly used in bijective encoding/decoding functions.
+    /// Base-16: hexadecimal.
     /// </summary>
-    public static class Alphabet
-    {
-        /// <summary>
-        /// Base-16: hexadecimal.
-        /// </summary>
-        public const string Base16 = "0123456789ABCDEF";
+    public const string Base16 = "0123456789ABCDEF";
 
-        /// <summary>
-        /// Base-26: alphabetical (A-Z).
-        /// </summary>
-        public const string Base26 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    /// <summary>
+    /// Base-26: alphabetical (A-Z).
+    /// </summary>
+    public const string Base26 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-        /// <summary>
-        /// Base-52: similar to base-62, but excluding vowels.
-        /// </summary>
-        public const string Base52 = "0123456789BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz";
+    /// <summary>
+    /// Base-52: similar to base-62, but excluding vowels.
+    /// </summary>
+    public const string Base52 = "0123456789BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz";
 
-        /// <summary>
-        /// Base-58: similar to base-62, but excluding characters that may look similar.
-        /// </summary>
-        public const string Base58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+    /// <summary>
+    /// Base-58: similar to base-62, but excluding characters that may look similar.
+    /// </summary>
+    public const string Base58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 
-        /// <summary>
-        /// Base-62: 0-9, A-Z, a-z.
-        /// </summary>
-        public const string Base62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    }
+    /// <summary>
+    /// Base-62: 0-9, A-Z, a-z.
+    /// </summary>
+    public const string Base62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 }
